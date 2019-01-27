dece80d446e5a869b53393cb8f5ff902b9978082
reilly999 <reillyafflerback@outlook.com>
2019-01-27 12:37:15 -0500

    Created Rectangles and found the distance from the center of the image.

--- src/main/java/frc/robot/subsystems/VisionSubsystem.java
+++ src/main/java/frc/robot/subsystems/VisionSubsystem.java
@@ -8,12 +8,14 @@
 package frc.robot.subsystems;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfPoint;
 import org.opencv.core.MatOfPoint2f;
+import org.opencv.core.Point;
 import org.opencv.core.RotatedRect;
 import org.opencv.core.Scalar;
 import org.opencv.core.Size;
@@ -24,6 +26,7 @@ import edu.wpi.cscore.CvSource;
 import edu.wpi.cscore.UsbCamera;
 import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.commands.BallVision;
 
 
@@ -43,8 +46,7 @@ public class VisionSubsystem extends Subsystem {
   CvSource hSVScaleOutputStream = CameraServer.getInstance().putVideo("Edges", resolutionWidth, resolutionHeight);
   CvSource blackWhiteOutputStream = CameraServer.getInstance().putVideo("BlackWhite", resolutionWidth, resolutionHeight);
   CvSource erodeDilateOutputStream = CameraServer.getInstance().putVideo("ErodeDilate", resolutionWidth, resolutionHeight);
-  CvSource grayScaleOutputStream = CameraServer.getInstance().putVideo("GrayScale", resolutionWidth, resolutionHeight);
-
+  CvSource contoursOutputStream = CameraServer.getInstance().putVideo("Rectangles", resolutionWidth, resolutionHeight);
 
   Mat originalImage = new Mat();
   Mat alteredImage = new Mat();
@@ -52,6 +54,9 @@ public class VisionSubsystem extends Subsystem {
   Mat grayscaleImage = new Mat();
 
 
+  public Mat hierarchy = new Mat(), mat = new Mat(), image = new Mat();
+  public List<MatOfPoint> finalContours = new ArrayList<>();
+  public List<RotatedRect> rects = new ArrayList<>();
 
 
   Size blurSize = new Size(9, 9);
@@ -92,13 +97,45 @@ public class VisionSubsystem extends Subsystem {
 
     //Makes a greyscale version of the original image for edge detection 5
     Imgproc.cvtColor(originalImage, grayscaleImage, Imgproc.COLOR_BGR2GRAY);
-    grayScaleOutputStream.putFrame(grayscaleImage);
+    //Dilate and erode to convers separate edges to continuous lines
+    Imgproc.dilate(grayscaleImage, grayscaleImage, Imgproc.getStructuringElement(Imgproc.MORPH_RECT, edgeDilateSize));
+    Imgproc.erode(grayscaleImage, grayscaleImage, Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(3, 3)));
+
+    //Erases the edge pixils from the color filtered image
+    Core.bitwise_not(grayscaleImage, grayscaleImage);
+    Core.bitwise_and(mat, grayscaleImage, mat);
+
+    finalContours.clear();
+    //FInds the outlines of the white rectangles of the current image
+    Imgproc.findContours(mat, finalContours, hierarchy, Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE);
+    rects.clear();
+    //Makes rectangle objects for each of the contours
+    for (int i = 0; i < finalContours.size(); i++) {
+      RotatedRect rect = Imgproc.minAreaRect(new MatOfPoint2f(finalContours.get(i).toArray()));
+      if (rect.size.height > 10 && rect.size.width > 10 && rect.angle < 10)
+        rects.add(rect);
+
+    }
+      //Finds the center of the rectangle
+      double y = rects.get(0).center.y;
+      double x = rects.get(0).center.x;
+      SmartDashboard.putNumber("X Value", x);
+      SmartDashboard.putNumber("Y Value", y);
+
+      Point[] vertices = new Point[4];
+      rects.get(0).points(vertices);
+      MatOfPoint points = new MatOfPoint(vertices);
+      Imgproc.drawContours(originalImage, Arrays.asList(points), -1, new Scalar(0, 255, 0), 5);
+      contoursOutputStream.putFrame(originalImage);
+
+      double distanceFromCenter = x-160;
+      double angleFromCenter = Math.atan(distanceFromCenter);
 
 
-    
-   
+
 
   }
 
+  
 
 }
