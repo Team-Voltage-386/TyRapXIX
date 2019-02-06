2e50dfc856e70c7ff7eb6505c276481bc0acf1df
ahill0310 <43358300+ahill0310@users.noreply.github.com>
2019-02-06 18:25:09 -0500

    Set up BeakSubsystem

--- src/main/java/frc/robot/subsystems/BeakSubsystem.java
+++ src/main/java/frc/robot/subsystems/BeakSubsystem.java
@@ -7,7 +7,9 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import frc.robot.RobotMap;
 
 /**
  * Add your docs here.
@@ -15,6 +17,28 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 public class BeakSubsystem extends Subsystem {
   // Put methods for controlling this subsystem
   // here. Call these from Commands.
+  public static DoubleSolenoid hatchCapture = new DoubleSolenoid(RobotMap.hatchCaptureOpen,
+      RobotMap.hatchCaptureClosed);
+
+  public BeakSubsystem() {
+  }
+
+  public enum HatchScoringStates {
+    beakOpen, beakRelease;
+  }
+
+  public void setState(HatchScoringStates in) {
+    switch (in) {
+    case beakOpen:
+      hatchCapture.set(DoubleSolenoid.Value.kForward);
+      break;
+    case beakRelease:
+      hatchCapture.set(DoubleSolenoid.Value.kReverse);
+      break;
+    default:
+      break;
+    }
+  }
 
   @Override
   public void initDefaultCommand() {
