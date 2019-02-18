1d5a3d0c05c8a697ec9d61eca0665f26d92c6ef6
ahill0310 <43358300+ahill0310@users.noreply.github.com>
2019-02-18 09:48:13 -0500

    Fixed LevelTwoLiftElevator  logic

--- src/main/java/frc/robot/commands/climb/LevelTwoLiftElevator.java
+++ src/main/java/frc/robot/commands/climb/LevelTwoLiftElevator.java
@@ -5,9 +5,6 @@ import frc.robot.Robot;
 
 public class LevelTwoLiftElevator extends Command {
 
-  private boolean currentState, prevState;
-  private double limitSwitchChanges;
-
   public LevelTwoLiftElevator() {
     // Use requires() here to declare subsystem dependencies
     // eg. requires(chassis);
@@ -17,26 +14,18 @@ public class LevelTwoLiftElevator extends Command {
   // Called just before this Command runs the first time
   @Override
   protected void initialize() {
-    limitSwitchChanges = 0;
   }
 
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
-    currentState = Robot.endgameClimbSubsystem.getElevatorLimitSwitch();
-    if (currentState != prevState) {
-      limitSwitchChanges++;
-    }
-
     Robot.endgameClimbSubsystem.setElevatorSpeed(1);
-
-    prevState = currentState;
   }
 
   // Make this return true when this Command no longer needs to run execute()
   @Override
   protected boolean isFinished() {
-    return limitSwitchChanges > 0 && !Robot.endgameClimbSubsystem.getElevatorLimitSwitch();
+    return !Robot.endgameClimbSubsystem.getElevatorLimitSwitch();
   }
 
   // Called once after isFinished returns true
