5263f2431679786921d3b4e5d720d91cb57765a1
Justin Foster <islandjustin@bellsouth.net>
2019-02-18 12:04:59 -0500

    removed smartdashboard deploy

--- src/main/java/frc/robot/commands/climb/ManualClimb.java
+++ src/main/java/frc/robot/commands/climb/ManualClimb.java
@@ -17,8 +17,8 @@ public class ManualClimb extends Command {
   @Override
   protected void execute() {
     Robot.endgameClimbSubsystem.setElevatorSpeed(OI.xboxManipControl.getRawAxis(OI.MANIP_LEFT_JOYSTICK_VERTICAL));
-    //SmartDashboard.putNumber("Left Joystick Value Manip",
-        OI.xboxManipControl.getRawAxis(OI.MANIP_LEFT_JOYSTICK_VERTICAL));
+    // SmartDashboard.putNumber("Left Joystick Value Manip",
+    // OI.xboxManipControl.getRawAxis(OI.MANIP_LEFT_JOYSTICK_VERTICAL));
     Robot.endgameClimbSubsystem.setClimbArmSpeeds(OI.xboxManipControl.getRawAxis(OI.MANIP_RIGHT_JOYSTICK_VERTICAL));
     if (OI.xboxManipControl.getRawButton(8)) {
       Robot.endgameClimbSubsystem.setElevatorWheelsSpeed(1);
