e589519239efd53fa6b51a2c062477d3cac0bd41
Justin Foster <islandjustin@bellsouth.net>
2019-02-19 20:35:37 -0500

    Working climb with shoulder

---
+++ src/main/java/frc/robot/commands/climb/SetManipArm.java
@@ -0,0 +1,34 @@
+package frc.robot.commands.climb;
+
+import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.Robot;
+import frc.robot.subsystems.ArmSubsystem.ElbowStates;
+import frc.robot.subsystems.ArmSubsystem.Levels;
+
+public class SetManipArm extends Command {
+  public SetManipArm() {
+    // requires(Robot.armSubsystem);
+  }
+
+  @Override
+  protected void initialize() {
+  }
+
+  @Override
+  protected void execute() {
+    Robot.armSubsystem.setLevel(Levels.finalClimb, ElbowStates.reset, 0, 0);
+  }
+
+  @Override
+  protected boolean isFinished() {
+    return false;
+  }
+
+  @Override
+  protected void end() {
+  }
+
+  @Override
+  protected void interrupted() {
+  }
+}
